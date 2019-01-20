c896200adbc18ad21e9d7fb93d4a9f07bf5cacf4
agrudkow <agrudkow@gmail.com>
2019-01-20 17:10:34 +0100

    Basic operations on file

--- vdisc.cpp
+++ vdisc.cpp
@@ -2,10 +2,34 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <fstream>
 
 #include "vdisc.h"
 
+#define BLOCK_SIZE 2048
+#define NAME_SIZE 256
+
 using namespace std;
+
+struct superBlock{
+  char name[NAME_SIZE];
+  unsigned int size;
+  unsigned int freeSpace;
+};
+
+struct node{
+  unsigned int nodeId;
+  char name[NAME_SIZE];
+  unsigned int fileSize;
+  unsigned int nextNode;
+  char in_use;
+  char is_last;
+  //2B - offset
+};
+
+struct dataBlock{
+  char data[BLOCK_SIZE];
+};
 
 //Conver string to integer
 constexpr unsigned int str2int(const char* str, int h = 0){
@@ -22,28 +46,67 @@ void interface() {
 
     switch (str2int((const char*)command.data())) {
       case str2int("exit"): loopStatus = false; break;
-      case str2int("print"): printf("Interface\n"); break;
-      default: continue;
+      case str2int("print"): printf("%lu\n", sizeof(node)); break;
+      case str2int("create"): createVDisc("dysk1.img", 1024); break;
+      case str2int("open"): openVDisc("dysk1.img"); break;
+      case str2int("delete"): deleteVDisc("dysk1.img"); break;
+      default: cout << "Invalid command \n"; continue;
     }
   }
 }
 
-void createVDisc(/* arguments */) {
+int createVDisc(string name, unsigned int size) {
+  unsigned int discSize = size, freeSpace = size - 1;
+  //creating a file of given size
+  ofstream vdisc(name, ios::binary | ios::out);
+  if (!vdisc) return 1;
+  //Setting secified size
+  vdisc.seekp(size - 1);
+  vdisc.write("", 1);
+
+  vdisc.seekp(0);
+
+  vdisc.write((char*)name.data(), NAME_SIZE);
+  vdisc.write((char*)&discSize, sizeof(unsigned int));
+  vdisc.write((char*)&freeSpace, sizeof(unsigned int));
+
+
+  vdisc.close();
+  return 0;
+}
+
+int openVDisc(string name) {
+  char discName[NAME_SIZE];
+  //char discSize[4];
+  //char discFreeSpace[4];
+  unsigned int discSize , freeSpace;
+
+  ifstream vdisc(name, ios::binary | ios::in);
+  if (!vdisc) return 1;
+
+  vdisc.read(discName, (int)NAME_SIZE);
+  vdisc.read((char*)&discSize, 4);
+  vdisc.read((char*)&freeSpace, 4);
+  cout << discSize << endl;
+  cout << freeSpace << endl;
+
+  vdisc.close();
+  return 0;
+}
+
+void closeVDisc(string name) {
   /* code */
 }
 
-void openVDisc(/* arguments */) {
-  /* code */
+int deleteVDisc(string name) {
+  if (remove((const char*)name.data()) != 0)
+    perror("Error deleting vdisc");
+  else
+    cout << "Vdisc successfully deleted" << endl;
+
+  return 0;
 }
 
-void closeVDisc(/* arguments */) {
-  /* code */
-}
-
-void deleteVDisc(/* arguments */) {
-  /* code */
-}
-
-void renameVDisc(/* arguments */) {
+void renameVDisc(string oldName, string newName) {
   /* code */
 }
--- vdisc.h
+++ vdisc.h
@@ -1,19 +1,34 @@
 #ifndef VDISC_H
 #define VDISC_H
 //This header file contain basic operations for handling our virtual disc
-
+/*------------------dics-structure-----------------------
+***Suberbolck
+*256B - disc name
+*4B - size
+*4B - free space
+***node
+*4B - node id
+*256B - file name
+*1B - in_use flag(can bo 0 - not in use, 1 - in use, begining of the file, or 2 - in use, continuation of the file)
+*1B -is_last flag, 1 if it is last node, 0 if not
+*4B - file size
+*4B - next node (if does not exist equals 0)
+*2b - offset
+*** data block
+*2kB
+*-------------------------------------------------------*/
 //user interface -- contain main loop of the menagement system
 void interface();
 
-void createVDisc(/* arguments */);
+int createVDisc(std::string name, unsigned int size);
 
-void openVDisc(/* arguments */);
+int openVDisc(std::string name);
 
-void closeVDisc(/* arguments */);
+void closeVDisc(std::string name);
 
-void deleteVDisc(/* arguments */);
+int deleteVDisc(std::string name);
 
-void renameVDisc(/* arguments */);
+void renameVDisc(std::string oldName, std::string newName);
 
 
 
